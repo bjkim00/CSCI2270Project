d5a4f32e139b7d866e8fd6394e2b2a808c26a832
rithwik-myla <49371891+rithwik-myla@users.noreply.github.com>
2019-04-18 22:37:49 -0600

    User Function addition
    Added user function and made changes to ensure it works properly

--- PriorityQueue/PriorityQueueTestDriver.cpp
+++ PriorityQueue/PriorityQueueTestDriver.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
@@ -14,11 +15,42 @@ cout << "2. Add a group to Priority Queue" << endl;
 cout << "3. Show next group in the queue" << endl;
 cout << "4. Serve Next group" << endl;
 cout << "5. Serve Entire Queue" << endl;
-cout << "6. Quit" << endl;
+cout<<"6. Print top N"<<endl;
+cout << "7. Quit" << endl;
+}
+
+RoadNode* orderedArr(PriorityQueue &queue, RoadNode v1[]){
+  //PriorityQueue queue1(size);
+  int count=0;
+  int count1=0;
+  //RoadNode v1[4000];
+  RoadNode temp;
+  while(!queue.isEmpty()){
+    temp = queue.peek();
+    v1[count1] = temp;
+    queue.dequeue();
+    count++;
+    count1++;
+    //queue1.enqueue(temp.sectionLength, temp.quality, temp.trafficVolume, temp.iri, temp.rut);
+  }
+  for(int i=0; i<count; i++){
+    queue.enqueue(v1[count-i-1].sectionLength, v1[count-i-1].quality, v1[count-i-1].trafficVolume, v1[count-i-1].iri, v1[count-i-1].rut);
+  }
+  return v1;
+}
+
+void printNode(RoadNode node){
+  cout << "Section Length:" << node.sectionLength << endl;
+  cout << "Road Quality:" << node.quality << endl;
+  cout << "Traffic Volume:"<< node.trafficVolume << endl;
+  cout << "Road IRI:"<< node.iri<<endl;
+  cout << "Repair RUT: "<< node.rut<<endl;
 }
 
 int main(int argc, char* argv[]){
-  PriorityQueue myQueue(stoi(argv[1]));
+  int queueSize = stoi(argv[1]);
+  PriorityQueue myQueue(queueSize);
+  RoadNode v[4000];
   int input;
   do{
     displayMenu();
@@ -56,8 +88,8 @@ int main(int argc, char* argv[]){
         string sectionLength;
         string quality;
         string trafficVolume;
-        string priority;
-        string severity;    // we need checks to make sure the input is valid
+        string _iri;
+        string _rut;    // we need checks to make sure the input is valid
 
         cout << "Enter Section Length (in meters):" << endl; //Idk what units
         cin>>sectionLength;
@@ -68,28 +100,27 @@ int main(int argc, char* argv[]){
         cout << "Enter Traffic Volume (0-100):" << endl;
         cin>>trafficVolume;
 
-        cout <<"Enter Road Priority (0-100):"<<endl;
-        cin>>priority;
+        cout <<"Enter IRI (0-100):"<<endl;
+        cin>>_iri;
 
-        cout<<"Enter Road Severity (Severe, Moderate, or Minor):"<<endl;
-        cin>>severity;
+        cout<<"Enter RUT (0-100):"<<endl;
+        cin>>_rut;
 
-        myQueue.enqueue(stof(sectionLength), stof(quality), stof(trafficVolume), stof(priority), severity);
+        myQueue.enqueue(stof(sectionLength), stoi(quality), stoi(trafficVolume), stoi(_iri), stoi(_rut));
         break;
       }
       case 3:
       {
-        cout << "Section Length:" << myQueue.peek().sectionLength << endl;
-        cout << "Road Quality:" << myQueue.peek().quality << endl;
-        cout << "Traffic Volume:"<< myQueue.peek().trafficVolume << endl;
-        cout << "Road Priority:"<<myQueue.peek().priority<<endl;
-        cout << "Repair Severity: "<<myQueue.peek().severity<<endl;
+        RoadNode temp = myQueue.peek();
+        if(!myQueue.isEmpty()){
+          printNode(temp);
+        }
         break;
       }
       case 4:
       {
         if(!myQueue.isEmpty()){
-          cout << "Section Length: " << myQueue.peek().sectionLength<< " , Priority: "<< myQueue.peek().priority << " , Traffic Volume: "<<myQueue.peek().trafficVolume<<" , Severity: " <<myQueue.peek().severity<<endl;
+          cout << "Section Length: " << myQueue.peek().sectionLength<< " , IRI: "<< myQueue.peek().iri << " , Traffic Volume: "<<myQueue.peek().trafficVolume<<" , RUT: " <<myQueue.peek().rut<<endl;
           myQueue.dequeue();
         }
         else{
@@ -109,10 +140,32 @@ int main(int argc, char* argv[]){
       }
       case 6:
       {
-          break;
+        int count = myQueue.currentQueueSize;
+        string n;
+        cout<<"How many roads would you like to print: ";
+        cin.ignore();
+        getline(cin, n);
+        orderedArr(myQueue, v);
+        for(int i = 0; i<stoi(n); i++){
+          if(count>0){
+            cout<<"Road Section: "<<i+1<<endl;
+            printNode(v[i]);
+            cout<<endl;
+            count--;
+          }
+          else{
+            cout<<"Queue empty no more to print."<<endl;
+            break;
+          }
+        }
+        break;
+      }
+      case 7:
+      {
+        break;
       }
     }
-  }while(input !=6);
+  }while(input !=7);
 
   cout<<"Goodbye!"<<endl;
   return 0;
