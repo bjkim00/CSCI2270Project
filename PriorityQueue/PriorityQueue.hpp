576977afee56faf28a826cb72c834d66ef6a558f
Byungjin Kim <43766458+bjkim00@users.noreply.github.com>
2019-04-20 20:52:44 -0600

    Update PriorityQueue.hpp

--- PriorityQueue/PriorityQueue.hpp
+++ PriorityQueue/PriorityQueue.hpp
@@ -7,12 +7,15 @@ using namespace std;
 // a struct to store customers group information
 struct RoadNode
 {
-    float sectionLength;
-    int quality;
-    int iri;
-    int rut;
-    int trafficVolume;
-    float priority;
+    string highway; //Hwy name
+    float sectionLength; //Length of the section that we are looking at
+    int quality; //This should be the condition of the road converted to int
+    int iri; //International roughness index (1-100)
+    int rut; //RUT value between (0-100)
+    float trafficVolume; //Traffic average
+    float priority; //Priority of each individual section, this will be calculated
+    //In calcPriority
+    float normPriority; //This is the normalized priority
     bool severity;
 };
 
@@ -30,8 +33,11 @@ class PriorityQueue
     // Purpose: Determine the priority of the highway
     float calcPriority(int _quality, int _trafficVolume, int _iri, int _rut);
 
+    // Purpose: Normalize the priority of the highways
+    void calcNormPriority();
+
     // Purpose: enqueue new group into priority queue; call other
-    void enqueue (float _sectionLength, int _quality, int _trafficVolume, int _iri, int _rut);
+    void enqueue (string _highway, float _sectionLength, int _quality, int _trafficVolume, int _iri, int _rut);
 
     //Purpose: remove the group (allocate the table) at the front of the priority queue from
     void dequeue();
@@ -56,6 +62,8 @@ class PriorityQueue
 
     RoadNode* priorityQueue; //pointer to the array used to implement priority queue
     int maxQueueSize; //maximum capacity of priority queue
+    float maxPriority; //maximum priority (no normalization)
+    float minPriority; //min priority (no normalization)
 };
 
 #endif
